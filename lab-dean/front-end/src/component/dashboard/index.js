8944a5b22a74b38cb7400126f011ed7e904a96e1
Dean <dean.murphy90@gmail.com>
2018-03-12 22:50:12 -0700

    copied folder over

---
+++ lab-dean/front-end/src/component/dashboard/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class Dashboard extends React.Component {
+  render() {
+    return (
+      <div className="dashboard-container">
+        <h1>Welcome to Your Dashboard!</h1>
+        <p>*</p>
+      </div>
+    );
+  }
+}
